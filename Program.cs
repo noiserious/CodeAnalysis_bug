b85ba225ee2d0c410235a6f242b221894b209732
Marino <marino@solidclouds.com>
2025-05-15 13:24:37 +0000

    Files that reproduce the bug.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+var text = File.ReadAllText("Code.txt");
+
+var tree = CSharpSyntaxTree.ParseText(text);
+
+File.WriteAllText("CodeBroken.cs", tree.GetCompilationUnitRoot().ToString());
